19b0eabec9343787f42f881f1af49acd5c7b531e
asrodionov <arodionov27@yandex.ru>
2020-09-13 17:14:19 +0300

    Add CI

--- src/test/java/ru/netology/web/test/TravelByInCreditTest.java
+++ src/test/java/ru/netology/web/test/TravelByInCreditTest.java
@@ -19,39 +19,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TravelByInCreditTest {
 
-    public static Stream<DataHelper.CardInfo> getValidCardData() {
-        return Stream.of(
-                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "ANDREI RODIONOV", "691"),
-                new DataHelper.CardInfo("01", DataHelper.getYearCard(1), "ANDREI RODIONOV", "999"),
-                new DataHelper.CardInfo("12", DataHelper.getYearCard(0), "ANDREI RODIONOV", "691"),
-                new DataHelper.CardInfo("09", DataHelper.getYearCard(0), "ANDREI RODIONOV", "111"));
-    }
-
-    public static Stream<DataHelper.CardInfo> getNotValidCardDataForValidityCardError() {
-        return Stream.of(
-                new DataHelper.CardInfo("00", DataHelper.getYearCard(1), "ANDREI RODIONOV", "691"),
-                new DataHelper.CardInfo("13", DataHelper.getYearCard(1), "ANDREI RODIONOV", "691"),
-                new DataHelper.CardInfo("06", DataHelper.getYearCard(8), "ANDREI RODIONOV", "691"));
-    }
-
-    public static Stream<DataHelper.CardInfo> getNotValidCardDataForOwnerCardError() {
-        return Stream.of(
-                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "АНДРЕЙ РОДИОНОВ", "691"),
-                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "123", "691"),
-                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "%&$", "691"));
-    }
-
     @BeforeAll
     static void setUpAll() {
         SelenideLogger.addListener("allure", new AllureSelenide().screenshots(true));
     }
+
     @AfterAll
     static void tearDownAll() {
         SelenideLogger.removeListener("allure");
     }
 
     @Test
-    void shouldSuccessCreditPurchaseWithGenerateData()  {
+    void shouldSuccessCreditPurchaseWithGenerateData() {
         val travelItemPage = open(System.getProperty("sut.url"), TravelItemPage.class);
         val byTravelPage = travelItemPage.byInCredit();
         byTravelPage.setValidNumberCard();
@@ -60,9 +39,17 @@ public class TravelByInCreditTest {
         assertEquals(SqlDataHelper.getStatusLastCreditTransaction(), "APPROVED");
     }
 
+    public static Stream<DataHelper.CardInfo> getValidCardData() {
+        return Stream.of(
+                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "ANDREI RODIONOV", "691"),
+                new DataHelper.CardInfo("01", DataHelper.getYearCard(1), "ANDREI RODIONOV", "999"),
+                new DataHelper.CardInfo("12", DataHelper.getYearCard(0), "ANDREI RODIONOV", "691"),
+                new DataHelper.CardInfo("09", DataHelper.getYearCard(0), "ANDREI RODIONOV", "111"));
+    }
+
     @ParameterizedTest
     @MethodSource("getValidCardData")
-    void shouldSuccessCreditPurchaseWithData(DataHelper.CardInfo cardInfo)  {
+    void shouldSuccessCreditPurchaseWithData(DataHelper.CardInfo cardInfo) {
         val travelItemPage = open(System.getProperty("sut.url"), TravelItemPage.class);
         val byTravelPage = travelItemPage.byInCredit();
         byTravelPage.setValidNumberCard();
@@ -79,6 +66,13 @@ public class TravelByInCreditTest {
         byTravelPage.setCardInfo(new DataHelper.CardInfo("07", DataHelper.getYearCard(1), "ANDREI RODIONOV", "691"));
         byTravelPage.checkErrorAlert();
         assertEquals(SqlDataHelper.getStatusLastCreditTransaction(), "DECLINED");
+    }
+
+    public static Stream<DataHelper.CardInfo> getNotValidCardDataForValidityCardError() {
+        return Stream.of(
+                new DataHelper.CardInfo("00", DataHelper.getYearCard(1), "ANDREI RODIONOV", "691"),
+                new DataHelper.CardInfo("13", DataHelper.getYearCard(1), "ANDREI RODIONOV", "691"),
+                new DataHelper.CardInfo("06", DataHelper.getYearCard(8), "ANDREI RODIONOV", "691"));
     }
 
     @ParameterizedTest
@@ -100,9 +94,16 @@ public class TravelByInCreditTest {
         byTravelPage.checkValidityYearCardError();
     }
 
+    public static Stream<DataHelper.CardInfo> getNotValidCardDataForOwnerCardError() {
+        return Stream.of(
+                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "АНДРЕЙ РОДИОНОВ", "691"),
+                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "123", "691"),
+                new DataHelper.CardInfo("06", DataHelper.getYearCard(1), "%&$", "691"));
+    }
+
     @ParameterizedTest
     @MethodSource("getNotValidCardDataForOwnerCardError")
-    void shouldErrorCreditPurchaseWithDataOwnerCardError(DataHelper.CardInfo cardInfo)  {
+    void shouldErrorCreditPurchaseWithDataOwnerCardError(DataHelper.CardInfo cardInfo) {
         val travelItemPage = open(System.getProperty("sut.url"), TravelItemPage.class);
         val byTravelPage = travelItemPage.byInCredit();
         byTravelPage.setValidNumberCard();
@@ -111,7 +112,7 @@ public class TravelByInCreditTest {
     }
 
     @Test
-    void shouldRightCreditIdInDb()  {
+    void shouldRightCreditIdInDb() {
         val travelItemPage = open(System.getProperty("sut.url"), TravelItemPage.class);
         val byTravelPage = travelItemPage.byInCredit();
         byTravelPage.setValidNumberCard();
